29734d88d88db7664f62785ff68c440671706eb3
Mukund Mauji <mauji.mukund@yahoo.ca>
2024-10-28 15:09:57 -0400

    add another page so we can test orbit controls

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-export default {
-  root: './src',
-  server: {
-    // host: true
-  }
-}
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+const root = resolve('./src');
+
+export default defineConfig({
+  root,
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        orbit: resolve(root, 'orbit/index.html'),
+      },
+    },
+  },
+})
