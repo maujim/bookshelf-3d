c7ca690f455fb512d41ba8b9c8aedc15f829d011
Mukund Mauji <mauji.mukund@yahoo.ca>
2024-11-02 17:52:55 -0400

    get a scalable grid of cubes working

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+import Box from './Box.jsx';
+import { Canvas, useFrame } from '@react-three/fiber';
+
+import * as THREE from 'three';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+
+  const scaling = 7;
+
+  const x = 3 * scaling;
+  const y = 3 * scaling;
+
+  const spacing = 1.5 / scaling;
+  const boxSize = 1 / scaling;
+
+  const isEven = k => k % 2 === 0;
+  const top = k => isEven(k) ? k / 2 :  (k - 1) / 2;
+  const bottom = k => -1 * top(k);
+
+  const grid = [];
+  for (let xx = bottom(x); xx <= top(x); xx++) {
+    for (let yy = bottom(y); yy <= top(y); yy++) {
+      grid.push([xx * spacing , yy * spacing , 0]);
+    }
+  }
+
+  console.log(grid);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Canvas style={{ height: window.innerHeight, width: window.innerWidth }}>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+
+        {grid.map(pos => (
+          <Box key={pos} position={pos} boxSize={boxSize} />
+        ))}
+      </Canvas>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Box.jsx
@@ -0,0 +1,31 @@
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef();
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta));
+  // Return the view, these are regular Threejs elements expressed in JSX
+
+  const boxSize = props.boxSize || 1;
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={event => click(!clicked)}
+      onPointerOver={event => hover(true)}
+      onPointerOut={event => hover(false)}
+    >
+      <boxGeometry args={Array(3).fill(boxSize)} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+}
+
+export default Box;
